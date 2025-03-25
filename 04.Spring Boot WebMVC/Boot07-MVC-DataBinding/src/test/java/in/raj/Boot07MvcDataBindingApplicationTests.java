764bb364da0d73b930b92fa0b2d569b54b20f9ea
RajYadav75 <rajyadav751811@gmail.com>
2025-03-25 08:39:08 +0530

    Data Binding

---
+++ 04.Spring Boot WebMVC/Boot07-MVC-DataBinding/src/test/java/in/raj/Boot07MvcDataBindingApplicationTests.java
@@ -0,0 +1,13 @@
+package in.raj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot07MvcDataBindingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
