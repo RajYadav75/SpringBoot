d15e685a262944381ac24551c147cfe734eb68ef
RajYadav75 <rajyadav751811@gmail.com>
2025-04-19 09:18:59 +0530

    Sending XML data as web url request

---
+++ 07.Spring Boot Rest/BootRest06-SendingXMLDataWithTheRequest/src/main/java/in/raj/BootRest06SendingXmlDataWithTheRequestApplication.java
@@ -0,0 +1,13 @@
+package in.raj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRest06SendingXmlDataWithTheRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRest06SendingXmlDataWithTheRequestApplication.class, args);
+    }
+
+}
