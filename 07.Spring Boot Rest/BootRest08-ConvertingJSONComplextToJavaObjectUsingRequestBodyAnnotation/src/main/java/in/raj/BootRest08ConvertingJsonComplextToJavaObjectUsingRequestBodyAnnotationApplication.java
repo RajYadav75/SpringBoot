470391db16bcfc5912beacd18a182efccb5c8fb3
RajYadav75 <rajyadav751811@gmail.com>
2025-04-21 09:57:27 +0530

    Converting JSON Complex data into Java Object Using @RequestBody Annotation

---
+++ 07.Spring Boot Rest/BootRest08-ConvertingJSONComplextToJavaObjectUsingRequestBodyAnnotation/src/main/java/in/raj/BootRest08ConvertingJsonComplextToJavaObjectUsingRequestBodyAnnotationApplication.java
@@ -0,0 +1,13 @@
+package in.raj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRest08ConvertingJsonComplextToJavaObjectUsingRequestBodyAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRest08ConvertingJsonComplextToJavaObjectUsingRequestBodyAnnotationApplication.class, args);
+    }
+
+}
