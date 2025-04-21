10e92b4fd9a94d7330769a77ff82ee088d9d94e5
RajYadav75 <rajyadav751811@gmail.com>
2025-04-21 09:48:52 +0530

    Sending JSON Data with The Request

--- 07.Spring Boot Rest/BootRest07-SendingJSONDataWithTheRequest/src/main/java/in/raj/rest/DostRestOperations.java
+++ 07.Spring Boot Rest/BootRest07-SendingJSONDataWithTheRequest/src/main/java/in/raj/rest/DostRestOperations.java
@@ -1,9 +1,21 @@
 package in.raj.rest;
 
+import in.raj.modal.Dost;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Dost-Api")
 public class DostRestOperations {
+    @PostMapping("/register")
+    public ResponseEntity<Dost> registerDost(@RequestBody Dost dost) {
+        System.out.println("DostRestOperations.registerDost");
+//        dost.setBillAmount(dost.getBillAmount()+1000.0f);
+        System.out.println(dost);
+        return new ResponseEntity<>(dost, HttpStatus.OK);
+    }
 }
