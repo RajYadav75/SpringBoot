90f9110ef78134431b88cf0d14baafe978444f28
RajYadav75 <rajyadav751811@gmail.com>
2025-04-22 09:54:15 +0530

    Converting XML Complex Data As Java Object Using @RequestBody

--- 07.Spring Boot Rest/BootRest09-ConvertingXMLComplexDataToJavaObject/src/main/java/in/raj/rest/CustomerOperationsController.java
+++ 07.Spring Boot Rest/BootRest09-ConvertingXMLComplexDataToJavaObject/src/main/java/in/raj/rest/CustomerOperationsController.java
@@ -17,4 +17,34 @@ public class CustomerOperationsController {
         System.out.println(customer);
         return new ResponseEntity<>(customer, HttpStatus.OK);
     }
+    /*
+    * <Customer>
+    <cno>101</cno>
+    <cname>John Doe</cname>
+    <cadd>456 Elm St, Springfield</cadd>
+    <compniesList>
+        <Company>
+            <id>1</id>
+            <name>Tech Innovators</name>
+            <addrs>Silicon Valley, CA</addrs>
+            <size>500</size>
+        </Company>
+        <Company>
+            <id>2</id>
+            <name>Global Solutions</name>
+            <addrs>London, UK</addrs>
+            <size>1200</size>
+        </Company>
+    </compniesList>
+    <familyIds>
+        <member>
+            <key>spouse</key>
+            <value>78965</value>
+        </member>
+        <member>
+            <key>child</key>
+            <value>45678</value>
+        </member>
+    </familyIds>
+</Customer>*/
 }
