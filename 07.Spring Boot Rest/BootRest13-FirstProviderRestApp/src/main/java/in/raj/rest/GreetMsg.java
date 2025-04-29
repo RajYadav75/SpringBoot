51a222c97d15d8a1934b4a83b2fc22a490e0ab61
RajYadav75 <rajyadav751811@gmail.com>
2025-04-29 09:50:30 +0530

    First Provider App Mean this is Rest Api

--- 07.Spring Boot Rest/BootRest13-FirstProviderRestApp/src/main/java/in/raj/rest/GreetMsg.java
+++ 07.Spring Boot Rest/BootRest13-FirstProviderRestApp/src/main/java/in/raj/rest/GreetMsg.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/wish")
+@RequestMapping("/Wish-Api")
 public class GreetMsg {
 
     @GetMapping("/greet")
