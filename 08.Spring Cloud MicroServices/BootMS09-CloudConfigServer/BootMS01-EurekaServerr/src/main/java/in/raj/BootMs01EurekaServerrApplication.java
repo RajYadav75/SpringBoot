761447ab9675f588a978b8d718c2cedafc7aee9b
RajYadav75 <rajyadav751811@gmail.com>
2025-05-17 08:58:59 +0530

    First Step:: Eureka Server Project created and implemented

---
+++ 08.Spring Cloud MicroServices/BootMS09-CloudConfigServer/BootMS01-EurekaServerr/src/main/java/in/raj/BootMs01EurekaServerrApplication.java
@@ -0,0 +1,15 @@
+package in.raj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BootMs01EurekaServerrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootMs01EurekaServerrApplication.class, args);
+    }
+
+}
