dda45c9ff76d4f78f5c7dc2e1cbea84636af64d3
RajYadav75 <rajyadav751811@gmail.com>
2025-05-20 08:44:23 +0530

    Student Microservices

---
+++ 08.Spring Cloud MicroServices/BootMS11-WorkingWith@RefreshScope/BootMS04-StudentApi/src/test/java/in/raj/BootMs04StudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.raj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMs04StudentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
