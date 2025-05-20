e88e22338c9c3daba9643cc734d767bb02d6d009
RajYadav75 <rajyadav751811@gmail.com>
2025-05-20 08:45:24 +0530

    Student Microservices

--- 08.Spring Cloud MicroServices/BootMS11-WorkingWith@RefreshScope/BootMS04-StudentApi/src/main/java/in/raj/rest/Student.java
+++ 08.Spring Cloud MicroServices/BootMS11-WorkingWith@RefreshScope/BootMS04-StudentApi/src/main/java/in/raj/rest/Student.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/Student-Api")
 public class Student {
     @Value("${dbuser}")
     private String user;
