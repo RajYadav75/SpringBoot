50b9be2d63d9d62ce4a3330de54295520c172c01
RajYadav75 <rajyadav751811@gmail.com>
2025-06-18 09:08:43 +0530

    Reciever Application PTP

--- 11.Spring Boot JMS-Messaging/01.JMS-App/BootJMS02-RecieverApp/src/main/java/in/raj/reciever/PTPRecieverApp.java
+++ 11.Spring Boot JMS-Messaging/01.JMS-App/BootJMS02-RecieverApp/src/main/java/in/raj/reciever/PTPRecieverApp.java
@@ -1,7 +1,13 @@
 package in.raj.reciever;
 
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PTPRecieverApp {
+
+    @JmsListener(destination = "testmq1")
+    public void readMsg(String msg){
+        System.out.println("Reciever received :: "+msg);
+    }
 }
