311d5bf1cc167da298971302ca17978caeb5d06c
RajYadav75 <rajyadav751811@gmail.com>
2025-06-18 09:17:12 +0530

    Sender App PTP

---
+++ 11.Spring Boot JMS-Messaging/02.JMS-App-Scheduling/BootJMS01-SenderApp/src/main/java/in/raj/sender/PTPSenderApp.java
@@ -0,0 +1,19 @@
+package in.raj.sender;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class PTPSenderApp implements CommandLineRunner {
+    @Autowired
+    private JmsTemplate jmsTemplate;
+    @Override
+    public void run(String... args) throws Exception {
+        jmsTemplate.send("rajmq",session -> session.createTextMessage("From the Sender :: "+new Date()));
+        System.out.println("Sender has sent the message");
+    }
+}
