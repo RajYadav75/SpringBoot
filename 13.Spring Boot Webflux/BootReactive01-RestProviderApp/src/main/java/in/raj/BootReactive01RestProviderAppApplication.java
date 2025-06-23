dfa82c637cc7f6f34fe7761d7b8112d37f38dc73
RajYadav75 <rajyadav751811@gmail.com>
2025-06-23 09:23:50 +0530

    Reactive Programming Implemented

---
+++ 13.Spring Boot Webflux/BootReactive01-RestProviderApp/src/main/java/in/raj/BootReactive01RestProviderAppApplication.java
@@ -0,0 +1,13 @@
+package in.raj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootReactive01RestProviderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootReactive01RestProviderAppApplication.class, args);
+    }
+
+}
