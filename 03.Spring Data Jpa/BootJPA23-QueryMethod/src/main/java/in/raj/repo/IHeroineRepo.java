d33e8864abc448bde01e273b0cc7bbb99d5ffdb7
RajYadav75 <rajyadav751811@gmail.com>
2025-10-14 23:15:18 +0530

    @Query का इस्तेमाल custom JPQL या SQL query चलाने के लिए किया जाता है ताकि हम अपनी जरूरत के हिसाब से data fetch कर सकें।

--- 03.Spring Data Jpa/BootJPA23-QueryMethod/src/main/java/in/raj/repo/IHeroineRepo.java
+++ 03.Spring Data Jpa/BootJPA23-QueryMethod/src/main/java/in/raj/repo/IHeroineRepo.java
@@ -6,11 +6,18 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface IHeroineRepo extends JpaRepository<Heroines,Integer> {
-//    @Query("select actoress from Heroines as actoress")
+public interface IHeroineRepo extends JpaRepository<Heroines, Integer> {
+    //    @Query("select actoress from Heroines as actoress")
 //    @Query("from Heroines")
     @Query("from Heroines actoress")
     public List<Heroines> getAllHeroines();
+    /*@Query(" from Heroines where fees between :start and :end ")
+	public Iterable<Heroines>  getHeroinesByFeesAmountRange(@Param("start") float startAmount,@Param("end") float endAmount);*/
+
+    //@Query(" from Heroines where fees between :startFees and :endFees ")
+    //@Query(" from Heroines as actress where actress.fees between :startFees and :endFees ")
+    @Query("from Heroines where fees between ?1 and ?2")
+    public Iterable<Heroines> getHeroinesByFeesRange1(Integer startFees, Integer endFees);
 
     @Query("from Heroines where fees between ?1 and ?2")
     public List<Heroines> getHeroinesByFeesRange(Integer startFees, Integer endFees);
@@ -18,5 +25,25 @@ public interface IHeroineRepo extends JpaRepository<Heroines,Integer> {
     @Query("from Heroines where fees between ?1 and ?2")
     public Iterable<Heroines> getHeroinesByFeesRange2(Integer startFees, Integer endFees);
 
+    @Query("select id,name,fees from Heroines where name in(:name1,:name2)")//TODO:- scalar -projection (selecting specific multiple col values)
+    public List<Object[]> getHeroinesData(String name1, String name2);
+
+    @Query("select name,description from Heroines where name like :initChar")//TODO:- scalar -projection (selecting specific single col values)
+    public List<String> getHeroinesNamesUsingFchar(String initChar);
+
+    @Query("from Heroines where name=:name")
+    public Heroines getHeroinesDescription(String name);
+
+    @Query("select name,fees from Heroines where  name=:name")
+    public Object getHeroinesDataUsingName(String name);
+
+    @Query("select name from Heroines  where fees=:fees")
+    public String fetchHeroinesNameUsingFees(Integer fees);
+
+    @Query("select count(distinct fees) from Heroines ")
+    public int fetchAllHeroinesCountUsingFees();
+
+    @Query("select count(*), max(fees),min(fees),avg(fees),sum(fees) from Heroines ")
+    public Object fetchHeroinesAggregateData();
 
 }
--- 03.Spring Data Jpa/BootJPA23-QueryMethod/src/main/java/in/raj/runner/SelectOperationRunner.java
+++ 03.Spring Data Jpa/BootJPA23-QueryMethod/src/main/java/in/raj/runner/SelectOperationRunner.java
@@ -1,9 +1,13 @@
 package in.raj.runner;
 
+import in.raj.entity.Heroines;
 import in.raj.repo.IHeroineRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class SelectOperationRunner implements CommandLineRunner {
@@ -16,5 +20,39 @@ public class SelectOperationRunner implements CommandLineRunner {
         heroineRepo.getHeroinesByFeesRange(670000,800000).forEach(System.out::println);
         System.out.println("============================================");
         heroineRepo.getHeroinesByFeesRange2(670000,800000).forEach(System.out::println);
+        System.out.println("============================================");
+        List<Object[]> heroinesData = heroineRepo.getHeroinesData("Ankita Tiwari", "Pallavi Tripathi");
+        heroinesData.forEach(row->{
+            for(Object value:row){
+                System.out.print(value+" ");
+            }
+            System.out.println();
+        });
+        System.out.println("============================================");
+        heroineRepo.getHeroinesNamesUsingFchar("A%").forEach(System.out::println);
+        /*//TODO- Or
+        List<String> heroinesNamesUsingFchar = heroineRepo.getHeroinesNamesUsingFchar("A%");
+        heroinesNamesUsingFchar.forEach(char->{
+            System.out.println(char);
+        });*/
+
+        System.out.println("============================================");
+        Heroines ankitaTiwari = heroineRepo.getHeroinesDescription("Ankita Tiwari");
+        System.out.println(ankitaTiwari );
+        System.out.println("============================================");
+        Object ankitaTiwari1 = heroineRepo.getHeroinesDataUsingName("Ankita Tiwari");
+        Object[] ankitaTiwari2 = (Object[]) ankitaTiwari1;
+        System.out.println("Heroines Details :: "+ Arrays.toString(ankitaTiwari2));
+        System.out.println("============================================");
+        System.out.println("Heroine Details :: "+heroineRepo.fetchHeroinesNameUsingFees(780000));
+        System.out.println("============================================");
+        System.out.println("Heroines Count :: "+heroineRepo.fetchAllHeroinesCountUsingFees());
+        System.out.println("============================================");
+        Object []obj = (Object[]) heroineRepo.fetchHeroinesAggregateData();
+        System.out.println("Heroines Count :: "+obj[0]);
+        System.out.println("Heroines maxFees :: "+obj[1]);
+        System.out.println("Heroines minFees :: "+obj[2]);
+        System.out.println("Heroines avgFees :: "+obj[3]);
+        System.out.println("Heroines sumFees :: "+obj[4]);
     }
 }
