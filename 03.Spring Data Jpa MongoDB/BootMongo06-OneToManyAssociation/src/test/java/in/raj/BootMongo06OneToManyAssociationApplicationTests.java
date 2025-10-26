2869633f79642d681866686e2d62a2136fc9e6b5
RajYadav75 <rajyadav751811@gmail.com>
2025-10-26 08:47:30 +0530

    One To Many Association

---
+++ 03.Spring Data Jpa MongoDB/BootMongo06-OneToManyAssociation/src/test/java/in/raj/BootMongo06OneToManyAssociationApplicationTests.java
@@ -0,0 +1,13 @@
+package in.raj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMongo06OneToManyAssociationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
